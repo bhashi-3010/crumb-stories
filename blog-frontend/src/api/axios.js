2b6da647354ce32c0cef25bc621e7e1e217aeed8
Bhashitha Thudumaladinne <bhashitharoyal@gmail.com>
2026-08-17 16:34:45 +0530

    Connect frontend to deployed backend

--- blog-frontend/src/api/axios.js
+++ blog-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://crumb-stories.onrender.com/api",
+    baseURL: "https://crumb-stories-2.onrender.com/api",
 });
 
 export default API;
