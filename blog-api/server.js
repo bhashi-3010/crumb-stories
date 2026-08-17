047ae4e93ff870644d2e146c7320b916c04d837d
Bhashitha Thudumaladinne <bhashitharoyal@gmail.com>
2026-08-17 14:20:07 +0530

    Final project updates

--- blog-api/server.js
+++ blog-api/server.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
     res.send("My Blog API");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
