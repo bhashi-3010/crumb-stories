7a42fe813aa159c0a6c8b2843f981f93e1d7f634
Bhashitha Thudumaladinne <bhashitharoyal@gmail.com>
2026-08-17 16:25:41 +0530

    Fix CORS for deployed frontend

--- blog-api/server.js
+++ blog-api/server.js
@@ -13,7 +13,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://crumb-stories-1.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true
+}));
+
+app.use(express.json());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
